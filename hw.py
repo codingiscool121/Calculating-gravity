b2cf7591462a1238ece538de51780ecc7f6fb404
Coder 119 <65823748+codingiscool121@users.noreply.github.com>
2021-08-09 09:45:25 -0400

    Update hw.py

--- hw.py
+++ hw.py
@@ -1,4 +1,8 @@
 import csv
+import plotly.express as pe
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import seaborn as sb
 import pandas as pd
 stars=[]
 with open('star_with_gravity.csv', 'r') as f:
@@ -28,4 +32,22 @@ print("kilo",kg)
 for i in radius:
     meters=i*(1.496e+11)
 print("meters",meters)
+
+starmass = []
+starradius = []
+starname = []
+for i in planetdata:
+    starmass.append(i[3])
+    starradius.append(i[4])
+    starname.append(i[1])
+
+planetgravity = []
+
+for index, name in enumerate(starname):
+    #6371000= Earth radius in meters
+    #5.972e24= Earth mass in kg
+    planetgravity.append(6.67e-11*(float(starmass[index])*5.972e+24)/(float(starradius[index])**2)*6371000*6371000)
+
+scatter = pe.scatter(x=starradius, y=starmass, size=planetgravity, title='Star Gravity by Name', hover_data=[starname])
+scatter.show()
 
